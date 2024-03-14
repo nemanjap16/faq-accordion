f40c6ebe9bfc3f5dd6895bd3788b00663d11beab
nemanjap16 <pnemanja183@gmail.com>
2024-03-14 12:24:38 +0100

    all done

--- src/accordion.ts
+++ src/accordion.ts
@@ -8,21 +8,6 @@ window.onload = () => {
   const accordionSvg: NodeListOf<HTMLElement> =
     document.querySelectorAll(".accordion-svg");
 
-  // IIFE
-  (() => {
-    accordionSvg.forEach((el) => {
-      if (el.classList.contains("open")) {
-        el.innerHTML = `
-        <svg xmlns="http://www.w3.org/2000/svg" width="26" height="27" fill="none" viewBox="0 0 30 31"><path fill="#301534" d="M15 3.313A12.187 12.187 0 1 0 27.188 15.5 12.2 12.2 0 0 0 15 3.312Zm4.688 13.124h-9.375a.938.938 0 0 1 0-1.875h9.374a.938.938 0 0 1 0 1.876Z"/></svg>
-        `;
-      } else {
-        el.innerHTML = `
-        <svg xmlns="http://www.w3.org/2000/svg" width="26" height="27" fill="none" viewBox="0 0 30 31"><path fill="#AD28EB" d="M15 3.313A12.187 12.187 0 1 0 27.188 15.5 12.203 12.203 0 0 0 15 3.312Zm4.688 13.124h-3.75v3.75a.938.938 0 0 1-1.876 0v-3.75h-3.75a.938.938 0 0 1 0-1.875h3.75v-3.75a.938.938 0 0 1 1.876 0v3.75h3.75a.938.938 0 0 1 0 1.876Z"/></svg>
-        `;
-      }
-    });
-  })();
-
   const setupSVG = () => {
     accordionSvg.forEach((el) => {
       if (el.classList.contains("open")) {
@@ -37,10 +22,14 @@ window.onload = () => {
     });
   };
 
-  const toggleClass = (id: number | string | undefined) => {
+  // IIFE
+  (() => {
+    setupSVG();
+  })();
+
+  const toggleClass = (id: string) => {
     accordionText.forEach((text) => {
       if (text.dataset.id == id) {
-        // text.classList.toggle("hidden");
         if (text.ariaHidden == "false") {
           text.setAttribute("aria-hidden", "true");
         } else {
@@ -62,7 +51,7 @@ window.onload = () => {
       }
 
       const el = button.children[0] as HTMLElement;
-      toggleClass(el.dataset.id);
+      toggleClass(el.dataset.id!);
       button.children[1].classList.toggle("open");
       setupSVG();
     });
